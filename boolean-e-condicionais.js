5278e7266b9b7543e7ebb98e2655eb04d5a30a78
José Carlos Araújo Pontes <jose.carlos.araujo07@aluno.ifce.edu.br>
2022-06-01 14:41:38 -0300

    Update boolean-e-condicionais.js

--- boolean-e-condicionais.js
+++ boolean-e-condicionais.js
@@ -6,3 +6,49 @@ if(possuiFaculdade) {
 } else {
   console.log('Não possui faculdade')
 }
+
+console.log('-=--=--=--=--=--=--=--=--=--=-')
+
+var minhaIdade = 21
+var idadePai = 64
+
+if(minhaIdade > idadePai) {
+  console.log('É maior')
+}
+if(minhaIdade === idadePai){
+  console.log('É igual')
+}
+else {
+  console.log('É menor')
+}
+
+// (5 - 2) && (5 - ' ') && (5 - 2);
+//   3     &&   true    &&    3   
+//   3
+
+var expression = (5 - 2) && (5 - ' ') && (5 - 2);
+console.log(expression)
+
+
+var nome = 'Andre', // true
+    idade = 28, // true
+    possuiDoutorado = false, // false
+    empregoFuturo, // false ( undefined )
+    dinheiroNaConta = 0 // false
+
+console.log(!!'Andre',!!28, !!possuiDoutorado, !! empregoFuturo, !!dinheiroNaConta)
+
+var brasil = 207
+var china = 1340 
+
+console.log(brasil === china)
+
+// if (('Gato' === 'gato') && (5 > 2)) {
+//         false           && true
+// }            false
+
+if (('Gato' === 'gato') && (5 > 2)) {
+  console.log('Verdadeiro')
+} else {
+  console.log('Falso')
+}
