4465990a153f5587129dbdde31f1ad1d07d1973a
José Carlos Araújo Pontes <jose.carlos.araujo07@aluno.ifce.edu.br>
2022-06-05 15:11:56 -0300

    update

--- Dom para iniciantes/o-que-e-o-dom.js
+++ Dom para iniciantes/o-que-e-o-dom.js
@@ -3,3 +3,17 @@ const tamanhoDoHref= window.location.href.length
 console.log(tamanhoDoHref)
 
 //AULA 0301 - parte 1 [OK]
+
+const y = document.querySelector('h1')
+
+console.log('Exercicios------------------')
+console.log(window.location.href)
+
+const classeAtiva = window.document.querySelector(' .ativo')
+console.log(classeAtiva)
+
+const navigatorLanguage = window.navigator.language
+console.log('A linguagem do navegador é',navigatorLanguage )
+
+const larguraDaPagina = document.body.offsetWidth
+console.log(larguraDaPagina)
