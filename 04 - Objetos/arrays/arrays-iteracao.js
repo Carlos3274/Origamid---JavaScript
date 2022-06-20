19fa18df6f9d19b9a1061cc7d74a9c40f819478e
José Carlos Araújo Pontes <jose.carlos.araujo07@aluno.ifce.edu.br>
2022-06-20 16:41:43 -0300

    Update arrays-iteracao.js

--- 04 - Objetos/arrays/arrays-iteracao.js
+++ 04 - Objetos/arrays/arrays-iteracao.js
@@ -10,3 +10,37 @@ const maiorValor = numeros.reduce((anterior, atual) => {
 },0)
 
 console.log(maiorValor)
+
+
+// reduce pode retornar objetos a cada iteração 
+aulas = [
+  {
+    nome: 'JS 1',
+    min: 25,
+  },
+  {
+    nome: 'Java',
+    min: 40,
+  },
+  {
+    nome: 'POO',
+    min: 8,
+  },
+  {
+    nome: 'NodeJS',
+    min: 4,
+  },
+]
+
+var acumulador = {}
+acumulador[0] = 'teste'
+
+const listaAulas = aulas.reduce((acumulador, aula, index) => {
+  acumulador[index] = aula.nome
+  return acumulador
+}, {})
+
+console.log(listaAulas)
+
+
+
