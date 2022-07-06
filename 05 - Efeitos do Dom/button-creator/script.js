927044c793f41980c6a419c09d0207594956b6ce
José Carlos Araújo Pontes <jose.carlos.araujo07@aluno.ifce.edu.br>
2022-07-06 15:17:28 -0300

    Update script.js

--- 05 - Efeitos do Dom/button-creator/script.js
+++ 05 - Efeitos do Dom/button-creator/script.js
@@ -4,7 +4,6 @@ const btn = document.querySelector('.btn')
 
 controles.addEventListener('change', handleChange)
 
-
 const handleStyle = {
   element: btn,
 
@@ -35,28 +34,44 @@ const handleStyle = {
   borderRadius(value) {
     this.element.style.borderRadius = value + 'px'
   },
-  
+
   fontFamily(value) {
-    this.element.style.fontFamily = value 
+    this.element.style.fontFamily = value
   },
 
   fontSize(value) {
     this.element.style.fontSize = value + 'rem'
-  },
+  }
 }
-
 
 function handleChange(event) {
   const name = event.target.name
-  const value = event.target.value 
+  const value = event.target.value
 
-  
   handleStyle[name](value)
   showCss()
-
+  saveValues(name, value)
 }
 
 function showCss() {
-  cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>')
+  cssText.innerHTML =
+    '<span>' + btn.style.cssText.split('; ').join(';</span><span>')
 }
 
+function setValues() {
+  const properties = Object.keys(localStorage)
+
+  properties.forEach(propertie => {
+    handleStyle[propertie](localStorage[propertie])
+
+    controles.elements[propertie].value = localStorage[propertie]
+    
+  })
+  showCss()
+}
+
+setValues()
+
+function saveValues(name, value) {
+  localStorage[name] = value
+}
